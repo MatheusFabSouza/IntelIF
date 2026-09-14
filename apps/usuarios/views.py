f703d89e800b4c253a0028c05b3198b1d2fa5d5a
lucasthierry9 <lucasthierry092@gmail.com>
2026-09-14 04:17:00 -0300

    sicronização rápida funcionando

--- apps/usuarios/views.py
+++ apps/usuarios/views.py
@@ -87,6 +87,12 @@ def google_callback(request):
 
     request.session["google_token"] = token
 
+    usuario = Usuario.objects.get(id=usuario_id)
+
+    quantidade = sincronizar_atividades_classroom(usuario, token)
+
+    print("ATIVIDADES SINCRONIZADAS NO LOGIN:", quantidade)
+    
     return redirect("/")
 
 def sincronizar_classroom(request):
